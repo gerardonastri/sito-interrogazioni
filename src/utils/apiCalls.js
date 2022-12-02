6b5f9845259897039ddfb2da3466228da6567c5f
gerardonastri <90633465+gerardonastri@users.noreply.github.com>
2022-12-02 10:15:37 +0100

    Update apiCalls.js

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const  BASE_URL = "http://localhost:8000/api/";
-// const BASE_URL = "https://sito-interrogazioni-backend.onrender.com/api/"
+//const  BASE_URL = "http://localhost:8000/api/";
+const BASE_URL = "https://sito-interrogazioni-backend.onrender.com/api/"
 
 export const axiosReq = axios.create({
     baseURL:  BASE_URL
